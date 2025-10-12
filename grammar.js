9049aaa7bbe784857d6b63f63d743de4344f12b8
Vespinian <vespinian@proton.me>
2025-10-12 15:28:32 -0400

    added preproc STR

--- grammar.js
+++ grammar.js
@@ -10,137 +10,144 @@ const deprecated = fs.readFileSync('./data/deprecated.txt').toString().split("\n
 
 
 module.exports = grammar({
-    name: 'ic10',
+  name: 'ic10',
 
-    extras: $ => [$._whitespace],
+  extras: $ => [$._whitespace],
 
-    word: $ => $.identifier,
+  word: $ => $.identifier,
 
-    rules: {
-        source_file: $ => alias(repeat($.line), $.program),
+  rules: {
+    source_file: $ => alias(repeat($.line), $.program),
 
-        line: $ => seq(
-            optional(choice($.instruction, $.label)),
-            optional($.comment),
-            $.newline,
-        ),
+    line: $ => seq(
+      optional(choice($.instruction, $.label)),
+      optional($.comment),
+      $.newline,
+    ),
 
-        label: $ => seq($.identifier, ':'),
+    label: $ => seq($.identifier, ':'),
 
-        newline: $ => choice('\n', '\r\n', '\r'),
+    newline: $ => choice('\n', '\r\n', '\r'),
 
-        _whitespace: $ => /[ \t]+/,
+    _whitespace: $ => /[ \t]+/,
 
-        comment: $ => seq(
-            /#.*/
-        ),
+    comment: $ => seq(
+      /#.*/
+    ),
 
-        instruction: $ => seq(
-            field('operation', choice($.operation, alias($.identifier, $.invalid_instruction))),
-            repeat(field('operand',
-                $.operand
+    instruction: $ => seq(
+      field('operation', choice($.operation, alias($.identifier, $.invalid_instruction))),
+      repeat(field('operand',
+        $.operand
+      ))
+    ),
+
+    operand: $ => choice($.register, $.device_spec, $.number, $.logicable, $.identifier),
+
+    identifier: $ => /[a-zA-Z_.][a-zA-Z0-9_.]*/,
+
+    register: $ => token(prec(5, choice(
+      'ra',
+      'sp',
+      seq(
+        repeat1('r'),
+        /[0-9]|1[0-7]/
+      )
+    ))),
+
+    network_index: $ => /[0-9]+/,
+
+    device_spec: $ => seq(
+      $.device,
+      optional(
+        seq(
+          ":",
+          $.network_index,
+        )
+      )
+    ),
+
+    device: $ => token(prec(5, seq(
+      'd',
+      choice(
+        'b',
+        /[0-5]/,
+        seq(
+          repeat1('r'),
+          /[0-9]|1[0-7]/
+        )
+      ),
+    ))),
+
+    constant: $ => choice(
+      ...constants
+    ),
+
+    preproc_string: $ => /[^"\n]*/,
+
+    hash_preproc: $ => seq(
+      "HASH(\"",
+      field('string', $.preproc_string),
+      "\")",
+    ),
+
+    str_preproc: $ => seq(
+      "STR(\"",
+      field('string', $.preproc_string),
+      "\")",
+    ),
+
+    number: $ => choice(
+      token(
+        choice(
+          seq(
+            optional('-'),
+            /[0-9]+/,
+            optional(seq(
+              '.',
+              /[0-9]+/
             ))
+          ),
+          seq("%", /[01_]+/),
+          seq("$", /[0-9a-fA-F_]+/),
         ),
+      ),
+      $.constant,
+      $.hash_preproc,
+      $.str_preproc,
+      $.enum
+    ),
 
-        operand: $ => choice($.register, $.device_spec, $.number, $.logicable, $.identifier),
+    operation: $ => choice(
+      ...ops
+    ),
 
-        identifier: $ => /[a-zA-Z_.][a-zA-Z0-9_.]*/,
+    logicable: $ => prec(5, choice(
+      $.logictype, $.logicslottype, $.batchmode, $.reagentmode, $.deprecated
+    )),
 
-        register: $ => token(prec(5,choice(
-            'ra',
-            'sp',
-            seq(
-                repeat1('r'),
-                /[0-9]|1[0-7]/
-            )
-        ))),
+    logictype: $ => prec(6, choice(
+      ...logictypes
+    )),
 
-        network_index: $ => /[0-9]+/,
+    logicslottype: $ => choice(
+      ...slotlogictypes
+    ),
 
-        device_spec: $ => seq(
-            $.device,
-            optional(
-                seq(
-                    ":",
-                    $.network_index,
-                )
-            )
-        ),
+    batchmode: $ => choice(
+      ...batchmodes
+    ),
 
-        device: $ => token(prec(5,seq(
-            'd',
-            choice(
-                'b',
-                /[0-5]/,
-                seq(
-                    repeat1('r'),
-                    /[0-9]|1[0-7]/
-                )
-            ),
-        ))),
+    reagentmode: $ => choice(
+      ...reagentmodes
+    ),
 
-        constant: $ => choice(
-            ...constants
-        ),
+    enum: $ => token(prec(5, choice(
+      ...enums
+    ))),
 
-        preproc_string: $ => /[^"\n]*/,
-
-        hash_preproc: $ => seq(
-            "HASH(\"",
-            field('string', $.preproc_string),
-            "\")",
-        ),
-
-        number: $ => choice(
-            token(
-                choice(
-                    seq(
-                        optional('-'),
-                        /[0-9]+/,
-                        optional(seq(
-                            '.',
-                            /[0-9]+/
-                        ))
-                    ),
-                    seq("%", /[01_]+/),
-                    seq("$", /[0-9a-fA-F_]+/),
-                ),
-            ),
-            $.constant,
-            $.hash_preproc,
-            $.enum
-        ),
-
-        operation: $ => choice(
-            ...ops
-        ),
-
-        logicable: $ => prec(5,choice(
-            $.logictype, $.logicslottype, $.batchmode, $.reagentmode, $.deprecated
-        )),
-
-        logictype: $ => prec(6,choice(
-            ...logictypes
-        )),
-
-        logicslottype: $ => choice(
-            ...slotlogictypes
-        ),
-
-        batchmode: $ => choice(
-            ...batchmodes
-        ),
-
-        reagentmode: $ => choice(
-            ...reagentmodes
-        ),
-
-        enum: $ => token(prec(5,choice(
-            ...enums
-        ))),
-
-        deprecated: $ => token(prec(5,choice(
-            ...deprecated
-        )))
-    }
+    deprecated: $ => token(prec(5, choice(
+      ...deprecated
+    )))
+  }
 });
